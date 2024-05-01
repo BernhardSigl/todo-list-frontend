8a51b86324c50df9a60fcca86dd4cbf5619963f4
Bernhard Sigl <b-sigl@live.de>
2024-05-01 16:47:08 +0200

    login page

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  login() {}
+}
